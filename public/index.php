22602224194ca4f1ae4e4a5d13125c7534caaca4
Jean Le Grand Bokassa <j_dabok@me.com>
2019-01-13 02:46:28 +0100

    Added a new attribut to Route.php and 2 new methods

--- public/index.php
+++ public/index.php
@@ -6,6 +6,37 @@
  * Time: 23:39
  */
 
-echo 'Hello from public';
+//echo 'REQUEST URL : ' . $_SERVER['QUERY_STRING'];
 
-echo 'REQUEST URL : ' . $_SERVER['QUERY_STRING'];
+require '../core/Router.php';
+
+$route = new Router();
+
+//echo get_class($route);
+
+//add the routes
+
+$route->add('', ['controller' => 'Home', 'action' => 'index']);
+$route->add('posts', ['controller' => 'Posts', 'action' => 'index']);
+$route->add('show_post', ['controller' => 'Posts', 'action' => 'show']);
+
+// Display the routing table
+
+/*
+echo '<pre>';
+var_dump($route->getRoutes());
+echo '</pre>';
+*/
+
+
+// Matches request routes
+
+$url = $_SERVER['QUERY_STRING'];
+
+if ($route->match($url)) {
+    echo '<pre>';
+    var_dump($route->getParams());
+    echo '</pre>';
+} else {
+    echo ' No route found for URL ' . $url;
+}
