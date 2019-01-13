d15e22cb7a3b6e049612e1abd5e1a847dcad6158
Jean Le Grand Bokassa <j_dabok@me.com>
2019-01-13 04:41:48 +0100

    Added the first controller for dispatcher test

---
+++ app/controller/PostController.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jlbokass
+ * Date: 13/01/2019
+ * Time: 04:28
+ */
+
+
+class Posts
+{
+    public function index()
+    {
+        echo 'Hello from the index action in the Posts ';
+    }
+
+    public function addNew()
+    {
+        echo 'hello from the addnew action in the Posts';
+    }
+}
