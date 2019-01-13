376ed0edb722537c7468ac64bc6994cdf5c6714c
Jean Le Grand Bokassa <j_dabok@me.com>
2019-01-13 03:09:53 +0100

    new Router.php xit regex

--- core/Router.php
+++ core/Router.php
@@ -8,32 +8,71 @@
 
 class Router
 {
+    /**
+     * @var array
+     */
     protected $routes = [];
 
+    /**
+     * @var
+     */
     protected $params;
 
+    /**
+     * @param $route
+     * @param $params
+     */
     public function add($route, $params)
     {
         $this->routes[$route] = $params;
     }
 
+    /**
+     * @return array
+     */
     public function getRoutes()
     {
         return $this->routes;
     }
 
+    /**
+     * @param $url
+     * @return bool
+     */
     public function match($url)
     {
+        /*
         foreach ($this->routes as $route => $params) {
             if ($url == $route) {
                 $this->params = $params;
                 return true;
             }
         }
+        */
+
+        // Match to fixed URL format /controller/action
+        $reg_exp = "/^(?P<controller>[a-z-]+)\/(?P<action>[a-z-]+)$/";
+
+        if (preg_match($reg_exp, $url, $matches)) {
+            // Get named capture group values
+            $params = [];
+
+            foreach ($matches as $key => $match) {
+                if (is_string($key)) {
+                    $params[$key] = $match;
+                }
+            }
+
+            $this->params = $params;
+            return true;
+        }
 
         return false;
     }
 
+    /**
+     * @return mixed
+     */
     public function getParams()
     {
         return $this->params;
